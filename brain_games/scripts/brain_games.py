99a00edf903ea688f5b8e1a3d8ee21576d7dc215
JMURv <architect.lock@outlook.com>
2022-08-17 17:31:53 +0300

    Game Engine 2.2

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 import prompt
 
+
 def main():
     print('Welcome to the Brain Games!')
     global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+
 
 if __name__ == '__main__':
     main()
